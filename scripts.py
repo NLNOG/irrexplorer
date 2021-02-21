4954bf533b862a44c3d1185410f8f6eb5498967b
Sasha Romijn <github@mxsasha.eu>
2021-02-21 22:41:53 +0100

    Add poetry scripts

---
+++ scripts.py
@@ -0,0 +1,24 @@
+# This is a temporary workaround till Poetry supports scripts, see
+# https://github.com/sdispater/poetry/issues/241.
+from subprocess import check_call
+
+
+def reformat() -> None:
+    check_call(
+        ["black", "-l 100", "irrexplorer/"],
+    )
+    check_call(
+        ["isort", "-l 100", "irrexplorer/"],
+    )
+
+
+def lint() -> None:
+    check_call(["flake8", "irrexplorer/"])
+
+
+def build() -> None:
+    check_call(["yarn", "--cwd", "frontend/", "build"])
+
+
+def frontend_install() -> None:
+    check_call(["yarn", "--cwd", "frontend/"])
