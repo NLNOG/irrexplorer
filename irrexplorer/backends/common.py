d52e889dd046d97f47d8cb0cfe71cea63a4eb289
Teun Vink <teun@teun.tv>
2021-11-30 22:11:33 +0100

    add custom user agent, fixes #112

--- irrexplorer/backends/common.py
+++ irrexplorer/backends/common.py
@@ -48,7 +48,7 @@ async def retrieve_url_text(url: str):
     Retrieve `url` and return the text in the response.
     Raises ImporterException if the response status is not 200.
     """
-    async with aiohttp.ClientSession() as session:
+    async with aiohttp.ClientSession(headers={"User-Agent": "IRR explorer"}) as session:
         async with session.get(url) as response:
             if response.status != 200:
                 raise ImporterError(f"Failed import from {url}: status {response.status}")
